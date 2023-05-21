bebc1c73f389020d75e0bf0c5ee85266ec26d18c
KiruiTito <titokirui402@gmail.com>
2023-05-21 23:40:01 +0300

    Completed assignmnent

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,3 +15,24 @@ class String
     self.split(/[.?!]/).count { |sentence| !sentence.empty? }
   end
 end
+
+
+examples 
+sentence = "This is a sentence."
+puts sentence.sentence?     # true
+puts sentence.question?     # false
+puts sentence.exclamation?  # false
+
+question = "Is this a question?"
+puts question.sentence?     # false
+puts question.question?     # true
+puts question.exclamation?  # false
+
+exclamation = "Wow!"
+puts exclamation.sentence?     # false
+puts exclamation.question?     # false
+puts exclamation.exclamation?  # true
+
+paragraph = "This is a paragraph. It has multiple sentences. Right?"
+puts paragraph.count_sentences  # 3
+
